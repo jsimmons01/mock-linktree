8a1e8cb00395f000d6e7b53c2ee148676562a1eb
Jahmella Williams <simmons.jahmella@gmail.com>
2022-11-24 19:11:07 -0400

    Initial Commit

---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+
+
+const Button = () => {
+  return (
+    <div>
+        <button>Click Me</button>
+        <button>Click Me</button>
+        <button>Click Me</button>
+        <button>Click Me</button>
+    </div>
+  )
+}
+
+export default Button
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+
+
+const Header = () => {
+  return (
+    <div><img src="many-faces.jpg" alt="Iman Alchemy"></img></div>
+  )
+}
+
+export default Header
